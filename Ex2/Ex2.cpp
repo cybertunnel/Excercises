7181932f262acd23474c7b8ff030d88a7b3576cd
Tyler Morgan <tylermorgan210@gmail.com>
2023-10-06 16:25:27 -0400

    Adding more to the exercise code

--- Ex2/Ex2.cpp
+++ Ex2/Ex2.cpp
@@ -1,64 +1,105 @@
 #include <iostream>
 #include <cstring>
 #include <typeinfo>
+#include <cmath>
+
+// The options for the menu
+enum MenuOption {
+    Addition,
+    Subtraction,
+    Multiplication,
+    Division,
+    Exponential,
+    SquareRoot,
+    Exit
+};
+
+// Displays the options available in the menu. Dependant on MenuOptions enum
+void displayMenu() {
+    std::cout << "=================================" << std::endl;
+    std::cout << "1.\tAddition" << std::endl;
+    std::cout << "2.\tSubtraction" << std::endl;
+    std::cout << "3.\tMultiplication" << std::endl;
+    std::cout << "4.\tDivision" << std::endl;
+    std::cout << "5.\tExponential" << std::endl;
+    std::cout << "6.\tSquare Root" << std::endl;
+    std::cout << "7.\tExit" << std::endl;
+    std::cout << "---------------------------------" << std::endl;
+
+    return;
+}
+
+void clearInputBuffer() {
+    std::cout << "Invalid input. Please enter a valid selection." << std::endl;
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
 
 int main () {
 
-    char menuOptions[][44] = {"Addition", "Subtraction", "Multiplication", "Division", "Exit"};
-    int menuSize = sizeof(menuOptions)/sizeof(menuOptions[0]);
     while (true) {
-
-        // Reset selection
+        
+        displayMenu();
         int selection = 0;
-
-        // Present menu
-        std::cout << "Please select an option from the menu:" << std::endl;
-        for (int i = 0; i < menuSize; i++) {
-            std::cout << i + 1<< ".\t" << menuOptions[i] << std::endl;
-        }
-
-        // Get user's selection
-        std::cout << "Enter your choice:";
+        std::cout << "Enter your menu selection: ";
         if (std::cin >> selection) {
-            if (selection > 5) {
-                std::cout << "Invalid selection" << std::endl;
-            } else if (selection == 5) {
+            MenuOption selectedOption = static_cast<MenuOption>(selection-1);
+
+            // Check if the user wants to exit before prompting for numeric inputs
+            if (selectedOption == Exit) {
                 std::cout << "Goodbye!" << std::endl;
                 exit(0);
-            } else {
-                int num1, num2;
-                std::cout << "Enter number 1:";
-                std::cin >> num1;
-
-                std::cout << "Enter number 2:";
-                std::cin >> num2;
-
-                double result;
-                switch (selection) {
-                    case 1:
-                        std::cout << "Result : " << num1 << " + " << num2 << " = " << num1 + num2 << std::endl;
-                        break;
-                    case 2:
-                        std::cout << "Result : " << num1 << " - " << num2 << " = " << num1 - num2 << std::endl;
-                        break;
-                    case 3:
-                        std::cout << "Result : " << num1 << " * " << num2 << " = " << num1 * num2 << std::endl;
-                        break;
-                    case 4:
-                        std::cout << "Result : " << num1 << " / " << num2 << " = " << num1 / num2 << std::endl;
-                        break;
-                    default:
-                        std::cout << "ERROR: Invalid selection";
-                        break;
-                }
             }
 
+            int num1, num2;
+
+            std::cout << "Please enter your first number: ";
+            if (std::cin >> num1) {
+
+                if (selectedOption == SquareRoot) {
+                    std::cout << "Result: Sqrt(" << num1 << ") = " << sqrt(num1) << std::endl;
+                } else {
+
+                    std::cout << "Please enter your second number: ";
+                    
+                    if (std::cin >> num2) {
+                        switch(selectedOption) {
+                            case Addition:
+                                std::cout << "Result: " << num1 << " + " << num2 << " = " << num1 + num2 << std::endl;
+                                break;
+                            case Subtraction:
+                                std::cout << "Result: " << num1 << " - " << num2 << " = " << num1 - num2 << std::endl;
+                                break;
+                            case Multiplication:
+                                std::cout << "Result: " << num1 << " * " << num2 << " = " << num1 * num2 << std::endl;
+                                break;
+                            case Division:
+                                if (num2 == 0) {
+                                    std::cout << "Error: Cannot divide by 0" << std::endl;
+                                } else {
+                                    std::cout << "Result: " << num1 << " / " << num2 << " = " << num1 / num2 << std::endl;
+                                }
+                                break;
+                            case Exponential:
+
+                                std::cout << "Result: " << num1 << "^" << num2 << " = " << pow(num1,num2) << std::endl;
+                                break;
+                            case Exit:
+                                std::cout << "Goodbye!";
+                                exit(0);
+                            default:
+                                clearInputBuffer();
+                                break;
+                        }
+                    } else {
+                        clearInputBuffer();
+                    }
+                }
+            } else {
+                clearInputBuffer();
+            }
         } else {
-            std::cout << "Invalid input. Please enter a valid selection." << std::endl;
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            clearInputBuffer();
         }
     }
-
-    std::cout << std::endl;
 }
